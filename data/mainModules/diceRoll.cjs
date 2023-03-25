4fcb9e3f493f8f5433f35d549d1387ba2483bab7
Xalos <thomcatg50@gmail.com>
2023-03-25 22:15:22 +0100

    Release...

--- data/mainModules/diceRoll.cjs
+++ data/mainModules/diceRoll.cjs
@@ -18,11 +18,32 @@ module.exports = {
 
         else throw ["ErrorReply","La valeur __**"+rollMsg+"**__ assignée au dé n'est pas valide",message];
     },
-    advanceRoll: async function (message) {
+    advanceRoll: async function (rollDataDB) {
 
-        //var rollMsg = await utils.cClear(message.content,/!roll |!r /);
+        if(dice.validate(rollDataDB[1])){
+            var rollData = dice.roll(rollDataDB[1]);
+            console.log(rollDataDB);    
+            let winFail = "Fail";
+
+            switch(rollDataDB[2]){
+                case "-" :
+                    if(rollDataDB[3] >= rollData.result)winFail = "Réussite";
+                    console.log("-");
+                break;
+                case "+" :
+                    if(rollDataDB[3] <= rollData.result)winFail = "Réussite";
+                    console.log("+");
+                break;
+            }
+
+            console.log([rollDataDB[1],rollData.rolled,rollData.result,winFail,rollDataDB[0],rollDataDB[4],rollDataDB[5]]);
+
+            return [rollDataDB[1],rollData.rolled,rollData.result,winFail,rollDataDB[0],rollDataDB[4],rollDataDB[5]]
+        }
         
-        //if(dice.validate())
+
+        //Si cette erreur ce trigger c'est qu'il y a un sacré problème
+        else throw ["ErrorReply","La valeur __**"+rollMsg+"**__ assignée au dé dan la statistique n'est pas valide",message];
         
     },
     diceTest: async function (message) {
